370ce2d3aa3cfbbcb13c243a7ecbaa766fdd542d
+ <+>
2024-09-26 20:40:38 +0300

    Functions: Continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun("hike"))
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork("swim"))
+
+function wrapAdjective(description = "*"){
+    return function(trait = "special"){
+        return `You are ${description}${trait}${description}!`
+    }
+}
+console.log(wrapAdjective("||")())
